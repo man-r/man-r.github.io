d1929fd5025331fb84e4ec33bd854d1ad992e488
man-r <man-r@github.com>
2017-11-15 12:04:27 +0300

    adding SAP_ABAP

---
+++ SAP_ABAP/test.js
@@ -0,0 +1,236 @@
+	{
+		"section": "ABAP",
+		"question":"1. Which of the following is/are the application areas of SAP ECC?",
+		"correctAns": 3,
+		"options": [
+			{"text":"Sales and distribution", "correct" : false},
+			{"text":"Human Resources management", "correct" : false},
+			{"text":"Industry solution", "correct" : false},
+			{"text":"Production Planning", "correct" : false},
+			{"text":"Workflow", "correct" : false}
+		]
+	},	
+	{
+		"section": "ABAP",
+		"question":"2. The deployment strategy for SAP solutions comprises which of the following?",
+		"correctAns": 3,
+		"options": [
+			{"text":"Deploy on Platform", "correct" : false},
+			{"text":"Deploy on Device", "correct" : false},
+			{"text":"Deploy on SOA", "correct" : false},
+			{"text":"Deploy on Premise", "correct" : false},
+			{"text":"Deploy on Demand", "correct" : false}
+		]
+	},	
+	{
+		"section": "ABAP",
+		"question":"3. SAP has characteristics that have made it a widely used business solution. Mark the statements that are TRUE?",
+		"correctAns": 3,
+		"options": [
+			{"text":"Integrates business functions and data across the enterprise", "correct" : false},
+			{"text":"Has limited level of adaptability or flexibility", "correct" : false},
+			{"text":"Offers solutions based on enterprise size and needs", "correct" : false},
+			{"text":"Suitable only for enterprises having more than 2500 users", "correct" : false},
+			{"text":"Is an easy to learn and use accounting system", "correct" : false}
+		]
+	},	
+	{
+		"section": "ABAP",
+		"question":"4. What are the different layers in the SAP ECC architecture?",
+		"correctAns": 3,
+		"options": [
+			{"text":"Presentation Layer", "correct" : false},
+			{"text":"Gateway Layer", "correct" : false},
+			{"text":"Application Layer", "correct" : false},
+			{"text":"Database Layer", "correct" : false},
+			{"text":"Internet Communication Layer", "correct" : false}
+		]
+	},	
+	{
+		"section": "ABAP",
+		"question":"5. Select the components that are the capabilities of SAP Netweaver.",
+		"correctAns": 3,
+		"options": [
+			{"text":"Identity Management", "correct" : false},
+			{"text":"Master Data Management", "correct" : false},
+			{"text":"Sales and Distribution", "correct" : false},
+			{"text":"Business Process Management", "correct" : false},
+			{"text":"ABAP / Java Server", "correct" : false}
+		]
+	},	
+	{
+		"section": "ABAP",
+		"question":"6. Which of the following is NOT part of the SAP GUI screen structure?",
+		"correctAns": 3,
+		"options": [
+			{"text":"Application tool bar", "correct" : false},
+			{"text":"Standard tool bar", "correct" : false},
+			{"text":"Remote tool bar", "correct" : false},
+			{"text":"Status bar", "correct" : false},
+			{"text":"Title bar ", "correct" : false}
+		]
+	},	
+	{
+		"section": "ABAP",
+		"question":"7. User’s logiŶ iŶto the SAP systeŵ usiŶg the credeŶtials assigŶed to theŵ. What do you mean by user context͟?", "correct" : false},
+			{"text":"Data that is commonly assigned for all SAP users who login", "correct" : false},
+			{"text":"It is always required during the execution of an ABAP program in the work process", "correct" : false},
+			{"text":"Contains the user data and authorization data", "correct" : false},
+			{"text":"Each user can open up to 9 external sessions", "correct" : false},
+			{"text":"Contains the data that is handled during program execution", "correct" : false}
+		]
+	},	
+	{
+		"section": "ABAP",
+		"question":"8. The ABAP AS has a component called the dispatcher. What are the functions of the dispatcher?",
+		"correctAns": 3,
+		"options": [
+			{"text":"A connection between the presentation layer and application layer", "correct" : false},
+			{"text":"Manages the buffer in the AS", "correct" : false},
+			{"text":"Assigns the request to an available work process", "correct" : false},
+			{"text":"Format conversion of data between the PS and the AS", "correct" : false},
+			{"text":"Organizes communication between the dialog instance and central instance", "correct" : false}
+		]
+	},	
+	{
+		"section": "ABAP",
+		"question":"9. What are components included in the structure of a work process?",
+		"correctAns": 3,
+		"options": [
+			{"text":"Screen processor", "correct" : false},
+			{"text":"ABAP processor", "correct" : false},
+			{"text":"Database interface", "correct" : false},
+			{"text":"Shared memory", "correct" : false},
+			{"text":"Virtual handler", "correct" : false}
+		]
+	},	
+	{
+		"section": "ABAP",
+		"question":"10. What do you mean by a dual stack in the SAP Netweaver AS?",
+		"correctAns": 3,
+		"options": [
+			{"text":"ABAP AS", "correct" : false},
+			{"text":"ABAP AS + Java AS", "correct" : false},
+			{"text":"ABAP AS + VB.NET", "correct" : false},
+			{"text":"Java AS", "correct" : false},
+			{"text":"VB.NET + Java AS", "correct" : false}
+		]
+	},	
+	{
+		"section": "ABAP",
+		"question":"Identify the key capabilities of SAP NetWeaver.",
+		"correctAns": 3,
+		"options": [
+			{"text":"Service-oriented architecture (SOA) middleware", "correct" : false},
+			{"text":"Security and Identity management", "correct" : false},
+			{"text":"Business intelligence", "correct" : false},
+			{"text":"Customer Relationship Management", "correct" : false},
+			{"text":"Supply Chain Management", "correct" : false}
+		]
+	},	
+	{
+		"section": "ABAP",
+		"question":"12. Identify the different installation options for SAP NetWeaver Application Server (AS).",
+		"correctAns": 3,
+		"options": [
+			{"text":"SAP NetWeaver AS ABAP", "correct" : false},
+			{"text":"SAP NetWeaver AS Java", "correct" : false},
+			{"text":"SAP NetWeaver AS ABAP + Java", "correct" : false},
+			{"text":"SAP NetWeaver AS C++", "correct" : false},
+			{"text":"SAP NetWeaver All-In-One ", "correct" : false}
+		]
+	},	
+	{
+		"section": "ABAP",
+		"question":"13. Which of these programming environments are supported on the Netweaver platform?",
+		"correctAns": 3,
+		"options": [
+			{"text":"C++", "correct" : false},
+			{"text":"Java", "correct" : false},
+			{"text":"COBOL", "correct" : false},
+			{"text":"ABAP", "correct" : false},
+			{"text":"Pascal", "correct" : false}
+		]
+	},	
+	{
+		"section": "ABAP",
+		"question":"14. Which of the given architecture represents a three-tier configuration",
+		"correctAns": 3,
+		"options": [
+			{"text":"Client, Server and Database, all three configured on one computer", "correct" : false},
+			{"text":"Client and Server, configured on one computer and Database configured on another computer", "correct" : false},
+			{"text":"Client configured on one computer and the Server and Database both configured on another computer", "correct" : false},
+			{"text":"Client, Server and Database, all three configured on separate computers", "correct" : false},
+			{"text":"Client and Database configured one one computer and the database configured on another computer", "correct" : false}
+		]
+	},	
+	{
+		"section": "ABAP",
+		"question":"15. What are the advantages of a three-tier client/server configuration as compared with a singletier or two-tier configuration?",
+		"correctAns": 3,
+		"options": [
+			{"text":"Simpler scalability", "correct" : false},
+			{"text":"Simpler administration", "correct" : false},
+			{"text":"Load balancing", "correct" : false},
+			{"text":"Low hardware cost", "correct" : false},
+			{"text":"Faster deployment of applications", "correct" : false}
+		]
+	},	
+	{
+		"section": "ABAP",
+		"question":"16. AN installed software system that provides a defined set of functions that are part of an SAP solution is referred to as:",
+		"correctAns": 3,
+		"options": [
+			{"text":"SAP Business Suite", "correct" : false},
+			{"text":"SAP System", "correct" : false},
+			{"text":"SAP Instance", "correct" : false},
+			{"text":"SAP Server", "correct" : false},
+			{"text":"SAP Product", "correct" : false}
+		]
+	},	
+	{
+		"section": "ABAP",
+		"question":"17. Which of these statements can be associated with a three-tier configuration?",
+		"correctAns": 3,
+		"options": [
+			{"text":"is the configuration where the database may be hosted on multiple servers", "correct" : false},
+			{"text":"the configuration allows distributing the application layer processes among several hosts", "correct" : false},
+			{"text":"If the number of users exceeds a certain upper limit, the application layer can be extended to offset the negative system performance", "correct" : false},
+			{"text":"If the number of users exceeds a certain upper limit, the presentation layer can be extended to offset the negative system performance", "correct" : false},
+			{"text":"is the configuration where the application server may be hosted on multiple hosts SAP Cert", "correct" : false}
+		]
+	},	
+	{
+		"section": "ABAP",
+		"question":"18. Mark the statements that are characteristic of an SAP Instance.",
+		"correctAns": 3,
+		"options": [
+			{"text":"combines SAP system components together to provide one or more services", "correct" : false},
+			{"text":"The services of an instance are started and stopped sequentially", "correct" : false},
+			{"text":"The services of an instance are started and stopped together", "correct" : false},
+			{"text":"Multiple instances may be configured on one physical computer", "correct" : false},
+			{"text":"SAP instance and Presentation Server are often used synonymously", "correct" : false}
+		]
+	},	
+	{
+		"section": "ABAP",
+		"question":"19. An instance that offers services that no other instance of the system offers is called the:",
+		"correctAns": 3,
+		"options": [
+			{"text":"Core Instance", "correct" : false},
+			{"text":"SAP Instance", "correct" : false},
+			{"text":"Dialog Instance", "correct" : false},
+			{"text":"Central Instance", "correct" : false},
+			{"text":"Primary Instance", "correct" : false}
+		]
+	},	
+	{
+		"section": "ABAP",
+		"question":"20. Which of the following is NOT an application from the SAP Business Suite?",
+		"correctAns": 3,
+		"options": [
+			{"text":"SAP ERP (SAP Enterprise Resource Planning)", "correct" : false},
+			{"text":"SAP CRM (SAP Customer Relationship Management)", "correct" : false},
+			{"text":"SAP BI (Business Intelligence)", "correct" : false},
+			{"text":"SAP SRM (SAP Supplier Relationship Management)", "correct" : false},
+			{"text":"SAP PLM (SAP Product Lifecycle Management)", "correct" : false}
