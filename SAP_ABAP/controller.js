d95b310f2240cc9c6d7fc48df250a9af4e3f2421
manar <man.rs85@gmail.com>
2017-11-26 14:03:10 +0300

    Update controller.js

--- SAP_ABAP/controller.js
+++ SAP_ABAP/controller.js
@@ -70,7 +70,7 @@ $(document).ready(function () {
 				correctAns.push(d_options[i].text);
 			}
 		}
-		var d_question = questionNumber + ". " + quizlistvarsion[questionNumber].question + " (" + correctAnsCount + " correct)";
+		var d_question = (1 + questionNumber) + ". " + quizlistvarsion[questionNumber].question + " (" + correctAnsCount + " correct)";
 		questionText.appendChild(document.createTextNode(d_question));
 		document.getElementById(htmlstage).insertBefore(questionText, document.getElementById(htmlstage).childNodes[0]);
 		
